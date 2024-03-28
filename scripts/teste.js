897d4bab542e2bb95a33224f326561c14fc75025
Gabriel <gabrielbtegani@gmail.com>
2024-03-28 09:24:40 -0300

    remove teste file

--- scripts/teste.js
+++
@@ -1,9 +0,0 @@
-let indices = [];
-let array = ["a", "b", "a", "c", "a", "d"];
-let elemento = "a";
-let idx = array.indexOf(elemento);
-while (idx != -1) {
-  indices.push(idx);
-  idx = array.indexOf(elemento, idx + 1);
-}
-console.log(indices);
