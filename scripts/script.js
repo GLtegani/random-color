084123b9c226e57e330f4e0e8b12729f9a9e591d
Gabriel <gabrielbtegani@gmail.com>
2024-03-28 08:05:45 -0300

    completed program

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,101 @@
 const bgBody = document.querySelector('body');
 const currentColorInput = document.querySelector('.color-input');
 const btnChange = document.querySelector('button');
+const normalSection = document.querySelector('.normal-section');
+const hexSection = document.querySelector('.hex-section');
+let randomNumber;
 let currentColor;
 
-let colorArray = [
-   'indigo',
-   'red',
-   'blue',
-   'purple',
-   'orange',
-   'pink',
-   'gray',
-   'white',
-   'yellow',
-   'green',
-   'black',
-   'aquamarine',
-   'brown',
-   'chartreuse',
-];
 
-let indexOfColorArray = [
+normalSection.onclick = () => {
    
-   colorArray.indexOf('indigo'),
-   colorArray.indexOf('red'),
-   colorArray.indexOf('blue'),
-   colorArray.indexOf('purple'),
-   colorArray.indexOf('orange'),
-   colorArray.indexOf('pink'),
-   colorArray.indexOf('gray'),
-   colorArray.indexOf('white'),
-   colorArray.indexOf('yellow'),
-   colorArray.indexOf('green'),
-   colorArray.indexOf('black'),
-   colorArray.indexOf('aquamarine'),
-   colorArray.indexOf('brown'),
-   colorArray.indexOf('chartreuse'),
-   
-];
-
-let numberOfColors = colorArray.length;
-
-
-btnChange.onclick = () => {
-   
-   let randomNumber = Math.round(Math.random() * numberOfColors);
+   btnChange.classList.remove('hide');
+   hexSection.classList.remove('active');
+   normalSection.classList.add('active');
    
    
-   if(randomNumber == indexOfColorArray[randomNumber]) {
+   let colorArray = [
+      'indigo',
+      'red',
+      'blue',
+      'purple',
+      'orange',
+      'pink',
+      'gray',
+      'white',
+      'yellow',
+      'green',
+      'black',
+      'aquamarine',
+      'brown',
+      'chartreuse',
+   ];
+   
+   let indexOfColorArray = [
       
-      currentColor = currentColorInput.style.color = colorArray[randomNumber];
-      currentColorInput.innerHTML = currentColor;
-      bgBody.style.backgroundColor = colorArray[randomNumber];
+      colorArray.indexOf('indigo'),
+      colorArray.indexOf('red'),
+      colorArray.indexOf('blue'),
+      colorArray.indexOf('purple'),
+      colorArray.indexOf('orange'),
+      colorArray.indexOf('pink'),
+      colorArray.indexOf('gray'),
+      colorArray.indexOf('white'),
+      colorArray.indexOf('yellow'),
+      colorArray.indexOf('green'),
+      colorArray.indexOf('black'),
+      colorArray.indexOf('aquamarine'),
+      colorArray.indexOf('brown'),
+      colorArray.indexOf('chartreuse'),
+      
+   ];
+   
+   let numberOfColors = colorArray.length;
+   
+   btnChange.onclick = () => {
+   
+      randomNumber = Math.round(Math.random() * numberOfColors);
+      
+      
+      if(randomNumber == indexOfColorArray[randomNumber]) {
+         
+         currentColor = currentColorInput.style.color = colorArray[randomNumber];
+         currentColorInput.innerHTML = currentColor;
+         bgBody.style.backgroundColor = colorArray[randomNumber];
+         
+      };
+      
       
    };
    
-   
 };
+
+hexSection.onclick = () => {
+   
+   btnChange.classList.remove('hide');
+   normalSection.classList.remove('active');
+   hexSection.classList.add('active');
+   
+   const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+   
+   
+   btnChange.onclick = () => {
+     
+      let hexColor = '#';
+      
+      for(let i = 0; i < 6; i++) {
+         randomNumber = Math.floor(Math.random() * hex.length);
+         hexColor += hex[randomNumber];
+      };
+      
+      currentColor = currentColorInput.style.color = hexColor;
+      currentColorInput.innerHTML = currentColor;
+      bgBody.style.backgroundColor = hexColor;
+      
+
+   };
+
+};
+
+
+
